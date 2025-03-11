b86489f3dd46f0e8017074b8d77e2596fad3af1f
Cesar Antonio mtz <92832278+CAMCDI@users.noreply.github.com>
2025-03-10 21:46:27 -0600

    Update regresion_lineal.py

--- regresion_lineal.py
+++ regresion_lineal.py
@@ -4,6 +4,7 @@ import numpy as np
 from pandasgui import show
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score, mean_squared_error
 
 
 # crear datos ficticios
@@ -15,7 +16,7 @@ fertilizante = np.random.uniform(0,100,n)
 
 #definir una relacion lineal con ruido aleatorio para la cantidad de papas
 #suponemos que por cada kg de fertilizante obtendremos 0.8 toneladas
-# y agregamos un oco de ruido normal para simular variedad normaal 
+# y agregamos un poco de ruido normal para simular variedad normaal 
 papas = 0.8 * fertilizante + np.random.normal(0,5,n) + 8 # intersecto en 8 
 
 df = pd.DataFrame ({
@@ -26,7 +27,7 @@ df = pd.DataFrame ({
 X = df[['Fertilizante' (kg)]].values
 Y = df['Papas (toneladas)'].values
 
-#separar los datos de netrenamiento y prueba
+#separar los datos de tratamiento y prueba
 X_train, X_test, y_train, y_test = train_test_split(x,y, test_size=0.2, random_state=0)
 
 #definir modelo y entrenarlo
@@ -35,6 +36,50 @@ regressor.fit(X_train,y_train)
 
 #hacer la prediccion 
 y_pred = regressor.predict(X_test)
+
+# calcular b0 y b1
+b1 = regressor.coef_[0]
+b0 = regressor.intercept_
+
+print(f"""
+    b1: {b1}
+    b0) {b0}
+""")
+
+# calcular mse. rmse, r^2
+mse = mean_squared_error(y_test,y_pred)
+rmse = np.sqrt(mse)
+r2 = r2_score(y_test, y_pred)
+
+print(f"""
+    mse: {mse}
+    rmse: {rmse}
+    r2: {r2}
+""")
+
+
+plt.figure(figsize=(10,6))
+
+# scatter plot del set de entrenamiento 
+
+plt.scatter(X_train,y_train, color="blue", label="datos de entrenamiento")
+
+# scatter plot para el set de prueba
+plt.scatter(X_test,y_test, color="green", label="Datos de prueba")
+
+plt.plot(X_train,regressor.predict(X_train), color="read", lable="linea de regresion")
+
+plt.title("regresion lineal: fertilizante vs produccion de papas")
+
+plt.xlabel("fertilizante (kg)")
+plt.ylabel("papas (toneladas)")
+plt.legend()
+
+plt.show()
+
+
+
+
 
 
 
